c9e34063e17b660689326c56cac8c0208be59b83
Melissa Gingold (student LM) <s011783@lm-19s011783.lmsd.org>
2019-02-01 10:21:35 -0500

    Initial Commit

---
+++ UNIHub beta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UNIHub beta
+//
+//  Created by Melissa Gingold (student LM) on 2/1/19.
+//  Copyright © 2019 Melissa Gingold (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
