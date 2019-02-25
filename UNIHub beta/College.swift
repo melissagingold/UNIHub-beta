a692f8ceefb3447d78e4261871794de31ccb2675
Niklas Pant (student LM) <s013442@lm-20s013442.lmsd.org>
2019-02-25 08:12:58 -0500

    updated college profiles

---
+++ UNIHub beta/College.swift
@@ -0,0 +1,23 @@
+//
+//  College.swift
+//  UNIHub beta
+//
+//  Created by Niklas Pant (student LM) on 2/21/19.
+//  Copyright © 2019 Melissa Gingold (student LM). All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class College {
+    
+    var name : String
+    var description : String
+    
+    init(name: String, description: String){
+        self.name = name
+        self.description = description
+    }
+    
+    
+}
