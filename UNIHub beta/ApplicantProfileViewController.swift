3fca8135c0b32077aa986970439af19a4b79a8e3
Melissa Gingold (student LM) <s011783@lm-19s011783.lmsd.org>
2019-02-26 08:39:30 -0500

    Tuesday Morning commit

--- UNIHub beta/ApplicantProfileViewController.swift
+++ UNIHub beta/ApplicantProfileViewController.swift
@@ -65,21 +65,39 @@ class ApplicantProfileViewController: UIViewController, UITextFieldDelegate, UIP
         return true
     }
     
-    @IBOutlet weak var addAPTestText: UITextField!
     
-    @IBOutlet weak var APTestList: UITextField!
+    //adding AP Tests
+    @IBOutlet weak var addAPTestText: UITextField!
+    @IBOutlet weak var APTestList2: UITextView!
     
     @IBAction func addAPTest(_ sender: UIButton) {
         if let text = addAPTestText.text{
             if text == ""{
                 return
             }
-            APTestList.text?.append("\(text)\n")
+            APTestList2.text?.append("\(text)\n")
             addAPTestText.text = ""
-            addAPTestText.resignFirstResponder()
+        addAPTestText.resignFirstResponder()
         }
-        
     }
+    
+    //adding SAT2 Tests
+    @IBOutlet weak var addSAT2Text: UITextField!
+    @IBOutlet weak var SAT2List: UITextView!
+    
+    @IBAction func addSAT2Test(_ sender: UIButton) {
+        if let text = addSAT2Text.text{
+            if text == ""{
+                return
+            }
+            SAT2List.text?.append("\(text)\n")
+            addSAT2Text.text = ""
+        addSAT2Text.resignFirstResponder()
+        }
+    }
+    
+    
+    
     
     
     
