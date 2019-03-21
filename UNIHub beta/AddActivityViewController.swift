b99f0c162ec968718785e374f9c0e013ad2b85b8
Melissa Gingold (student LM) <s011783@lm-19s011783.lmsd.org>
2019-03-21 10:32:54 -0400

    worked on adding activities

---
+++ UNIHub beta/AddActivityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddActivityViewController.swift
+//  UNIHub beta
+//
+//  Created by Melissa Gingold (student LM) on 3/21/19.
+//  Copyright © 2019 Melissa Gingold (student LM). All rights reserved.
+//
+
+import UIKit
+
+class AddActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
