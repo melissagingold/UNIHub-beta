c0b389a82ab525183f2c1f9e4121c2dd33cb6011
CharlieHerrmann <s011753@students.lmsd.org>
2019-04-10 01:24:16 -0400

    calendar

---
+++ UNIHub beta/AddCalendarTaskViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddCalendarTaskViewController.swift
+//  UNIHub beta
+//
+//  Created by Charles Herrmann (student LM) on 4/10/19.
+//  Copyright © 2019 Melissa Gingold (student LM). All rights reserved.
+//
+
+import UIKit
+
+protocol AddCalendarTask {
+    func addCalendarTask(id: String, task: String)
+}
+
+class AddCalendarTaskViewController: UIViewController {
+    var key = ""
+    @IBOutlet weak var taskText: UITextField!
+    var delegate: AddCalendarTask?
+    
+    
+    
+    @IBAction func enter(_ sender: UIButton) {
+        delegate?.addCalendarTask(id: key, task: taskText.text!)
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
