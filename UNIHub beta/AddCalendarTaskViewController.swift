c01a93a835f97c3b97fa22a2a380e0deda17d16f
CharlieHerrmann <s011753@students.lmsd.org>
2019-04-20 19:28:53 -0400

    slight edit to calendar

--- UNIHub beta/AddCalendarTaskViewController.swift
+++ UNIHub beta/AddCalendarTaskViewController.swift
@@ -12,22 +12,49 @@ protocol AddCalendarTask {
     func addCalendarTask(id: String, task: String)
 }
 
-class AddCalendarTaskViewController: UIViewController {
+class AddCalendarTaskViewController: UIViewController, UITextViewDelegate {
     var key = ""
     
     @IBOutlet weak var taskText: UITextView!
     var delegate: AddCalendarTask?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        taskText.text = "Add new task..."
+        taskText.textColor = UIColor.lightGray
+        taskText.font = UIFont(name: "Helvetica Neue", size: 22.0)
+        taskText.returnKeyType = .done
+        taskText.delegate = self
+        
+    }
 
     @IBAction func enter(_ sender: UIButton) {
         delegate?.addCalendarTask(id: key, task: taskText.text!)
         navigationController?.popViewController(animated: true)
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if taskText.text == "Add new task..." {
+            taskText.text = ""
+            taskText.textColor = UIColor.black
+            taskText.font = UIFont(name: "Helvetica Neue", size: 22.0)
+        }
     }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            taskText.resignFirstResponder()
+        }
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if taskText.text == "" {
+            taskText.text = "Add new task..."
+            taskText.textColor = UIColor.lightGray
+            taskText.font = UIFont(name: "Helvetica Neue", size: 22.0)
+        }
+    }
+    
+   
     
 }
