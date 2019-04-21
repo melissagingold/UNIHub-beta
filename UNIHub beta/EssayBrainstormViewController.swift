2b54ebbe4ce5a6fb3298c6d3037f86fb37e3ddaf
Chloe Cowan <s012524@students.lmsd.org>
2019-04-21 17:00:44 -0400

    supplementals for essay view controller

--- UNIHub beta/EssayBrainstormViewController.swift
+++ UNIHub beta/EssayBrainstormViewController.swift
@@ -13,6 +13,42 @@ class EssayBrainstormViewController: UIViewController {
     var getName = String()
 
     @IBOutlet weak var essayListName: UILabel!
+    @IBOutlet weak var essaySlider: UISlider!
+    @IBOutlet weak var suppLabel: UILabel!
+    
+    @IBOutlet weak var supp1Constraint: NSLayoutConstraint!
+    @IBOutlet weak var supp2Constraint: NSLayoutConstraint!
+    @IBOutlet weak var supp3Constraint: NSLayoutConstraint!
+    @IBOutlet weak var supp4Constraint: NSLayoutConstraint!
+    @IBOutlet weak var supp5Constraint: NSLayoutConstraint!
+    
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        suppLabel.text = String(Int(sender.value))
+        
+        if Int(sender.value) == 1 {
+            supp1Constraint.constant = 20
+            supp1Constraint.isActive = true
+        }
+        else if Int(sender.value) == 2 {
+            supp2Constraint.constant = 20
+            supp2Constraint.isActive = true
+        }
+        else if Int(sender.value) == 3 {
+            supp3Constraint.constant = 20
+            supp3Constraint.isActive = true
+        }
+        else if Int(sender.value) == 4 {
+            supp4Constraint.constant = 20
+            supp4Constraint.isActive = true
+        }
+        else if Int(sender.value) == 5 {
+            supp5Constraint.constant = 20
+            supp5Constraint.isActive = true
+        }
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
